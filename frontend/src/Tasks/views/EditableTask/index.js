847c38a28800d8cffabfde88d7d730f93ddd4796
apollo812 <goodluckbusiness0101@gmail.com>
2023-11-22 13:11:57 -0500

    fix: create card feature

--- frontend/src/Tasks/views/EditableTask/index.js
+++ frontend/src/Tasks/views/EditableTask/index.js
@@ -40,7 +40,7 @@ export default function EditableTask({ task, stage, removeTask, updateTask }) {
       updateTask({
         taskID: result.data.createCard.card.id,
         text: result.data.createCard.card.text,
-        stage: result.data.createCard.card.list
+        stage: result.data.createCard.card.listId
       });
       
     } catch (error) {
