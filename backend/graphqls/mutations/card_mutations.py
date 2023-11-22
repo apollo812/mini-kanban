82e6d86822aedc2b97338bb6f8a886be9011c680
apollo812 <goodluckbusiness0101@gmail.com>
2023-11-22 14:11:31 -0500

    refactor: create card feature with index

--- backend/graphqls/mutations/card_mutations.py
+++ backend/graphqls/mutations/card_mutations.py
@@ -3,6 +3,7 @@ from models.card_model import CardModel
 from ..schemas.card_schema import CardType
 from database.dynamodb_client import get_dynamodb_client
 from utils.time import time2graphql
+from boto3.dynamodb.conditions import Attr
 
 class CreateCard(graphene.Mutation):
     class Arguments:
@@ -17,6 +18,11 @@ class CreateCard(graphene.Mutation):
         dynamodb = get_dynamodb_client(local=True)
         table = dynamodb.Table('Cards')
         current_datetime = time2graphql()
-        table.put_item(Item={'id': id, 'key': id, 'listId': listId, 'index': 0, 'text': text, 'editMode': False, 'created': current_datetime, 'updated': current_datetime})
-        return CreateCard(card=CardModel(id, id, listId, 0, text, False, current_datetime, current_datetime))
+
+        card_cnt = len(table.scan(
+            FilterExpression=Attr('listId').eq(listId)
+        )['Items'])
+
+        table.put_item(Item={'id': id, 'key': id, 'listId': listId, 'index': card_cnt, 'text': text, 'editMode': False, 'created': current_datetime, 'updated': current_datetime})
+        return CreateCard(card=CardModel(id, id, listId, card_cnt, text, False, current_datetime, current_datetime))
 
