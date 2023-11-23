0eb0ba87aa33fbfa8246fa1f6632982d2bf95770
apollo812 <goodluckbusiness0101@gmail.com>
2023-11-22 21:36:31 -0500

    feat: backend delete list feature

--- backend/graphqls/mutations/mutations.py
+++ backend/graphqls/mutations/mutations.py
@@ -1,9 +1,10 @@
 import graphene
-from .list_mutations import CreateList
+from .list_mutations import CreateList, DeleteList
 from .card_mutations import CreateCard, CardIndexDrag, CardIndexDragToOther, DeleteCard
 
 class Mutation(graphene.ObjectType):
     create_list = CreateList.Field()
+    delete_list = DeleteList.Field()
     create_card = CreateCard.Field()
     card_index_drag = CardIndexDrag.Field()
     card_index_drag_to_other = CardIndexDragToOther.Field()
