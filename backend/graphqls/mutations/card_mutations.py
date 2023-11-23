866391e84bcdfa1c338a299b7a1a95cbbca3ba7a
apollo812 <goodluckbusiness0101@gmail.com>
2023-11-23 12:45:38 -0500

    fix: return invalid value of create card feature

--- backend/graphqls/mutations/card_mutations.py
+++ backend/graphqls/mutations/card_mutations.py
@@ -30,11 +30,11 @@ class CreateCard(graphene.Mutation):
         if response:  # If the response is not empty update state
             table.put_item(Item={'id': id, 'key': id, 'listId': listId, 'index': response[0]['index'], 'text': text, 'editMode': False, 'created': response[0]['created'], 'updated': current_datetime})
             print("Response is not empty! 😊")
+            return CreateCard(card=CardModel(id, id, listId, response[0]['index'], text, False, current_datetime, current_datetime))
         else:   #create state
             table.put_item(Item={'id': id, 'key': id, 'listId': listId, 'index': card_cnt, 'text': text, 'editMode': False, 'created': current_datetime, 'updated': current_datetime})
-
+            return CreateCard(card=CardModel(id, id, listId, card_cnt, text, False, current_datetime, current_datetime))
         
-        return CreateCard(card=CardModel(id, id, listId, card_cnt, text, False, current_datetime, current_datetime))
 
 class DeleteCard(graphene.Mutation):
     class Arguments:
